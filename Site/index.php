cc71f92c0314e6dcf99269d0f567ad2c1c54510b
Alex Vasil <alexandervasil840@gmail.com>
2022-07-15 17:17:04 -0400

    test without db connection

--- Site/index.php
+++ Site/index.php
@@ -7,13 +7,13 @@ $DBName = 'DiliTrust';
 $username = 'root';
 $password = 'admin'
 
-try {
-    $conn = new \PDO("mysql:host=$host;dbname=$DBName;charset=utf-8;", $username, $password);
-    $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
-    echo "Connected Successfully!";
-} catch (PDOException $e) {
-    echo "Connection Failed: " . $e->getMessage();
-}
+// try {
+//     $conn = new \PDO("mysql:host=$host;dbname=$DBName;charset=utf-8;", $username, $password);
+//     $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
+//     echo "Connected Successfully!";
+// } catch (PDOException $e) {
+//     echo "Connection Failed: " . $e->getMessage();
+// }
 
 echo "Hello World! From PHP!";
 
