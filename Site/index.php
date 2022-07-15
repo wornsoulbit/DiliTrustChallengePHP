51e165116569c73708523d42a63d8ccb02855c33
Alex Vasil <alexandervasil840@gmail.com>
2022-07-15 17:22:21 -0400

    test

--- Site/index.php
+++ Site/index.php
@@ -4,25 +4,6 @@
         <title>temp login page</title>
     </head>
     <body>
-        <?php
-
-        //Database connection.
-        $host = '127.0.0.1';
-        $DBName = 'DiliTrust';
-        $username = 'root';
-        $password = 'admin'
-        echo "Test before try";
-        try {
-            $conn = new \PDO("mysql:host=$host;dbname=$DBName;charset=utf-8;", $username, $password);
-            $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
-            echo "Connected Successfully!";
-        } catch (PDOException $e) {
-            echo "Connection Failed: " . $e->getMessage();
-        }
-
-        echo "Hello World! From PHP!";
-
-        ?>
         <form method="post" action="">
             <label>Username: <input type="text" name="username" /></label><br />
             <label>Password: <input type="password" name="password" /></label><br />
