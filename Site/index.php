9324488028ed855d49f0cb210992c8e020746be3
Alex Vasil <alexandervasil840@gmail.com>
2022-07-15 18:30:13 -0400

    Updated registered and login pages

--- Site/index.php
+++ Site/index.php
@@ -1,21 +1,5 @@
 <!DOCTYPE html>
 <?php 
-    if (isset($_POST['Login'])) {
-        echo "Login Attempt";
-        // Call login verification function
-
-        if (verifyLogin()) {
-            // Login to the user
-            echo "Welcome user";
-            // Create a session token.
-        } else {
-            // Don't log in.
-            echo "Invalid username/password";
-        }
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        echo "username: " . $username . " Password: " . $password;
-    }
 
     $servername = "localhost";
     $DBName = 'DiliTrust';
@@ -26,13 +10,39 @@
     $conn = new PDO("mysql:host=$servername;dbname=$DBName", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // echo "Connected successfully";
+        echo "Connected successfully\n";
     } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+        echo "Connection failed: " . $e->getMessage() . "\n";
+    }
+
+    if (isset($_POST['Login'])) {
+        echo "Login Attempt\n";
+        // Call login verification function
+
+        if (verifyLogin()) {
+            // Login to the user
+            echo "Welcome user\n";
+            // Create a session token.
+        } else {
+            // Don't log in.
+            echo "Invalid username/password\n";
+        }
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        echo "username: " . $username . " Password: " . $password . "\n";
     }
 
     function verifyLogin() {
+        // TODO: Retrieve data from db, verify username and password hash.
+        // session_start();
         return false;
+    }
+
+    function findUsername($username) {
+        $stmt = self::$conn->prepare("SELECT * FROM Profile WHERE username = :username");
+        $stmt->execute(['username'] => $username);
+        $stmt->setFetchMode(\PDO::FETCH_COLUMN);
+        return $stmt->fetch();
     }
 ?>
 <head> 
