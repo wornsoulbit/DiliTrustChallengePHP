f6c2ec9f918fa01d7052d4fbade3cc45f856b672
Alex Vasil <alexandervasil840@gmail.com>
2022-07-15 16:54:48 -0400

    added some more html

--- Site/index.php
+++ Site/index.php
@@ -19,5 +19,21 @@ echo "Hello World! From PHP!";
 ?>
 
 <html>
-<body> Hello World! </body>
+    <head>
+        <title>Log into an account</title>
+    </head>
+    <body>
+        <?php
+        if (isset($_GET['error']))
+            echo $_GET['error'];
+        ?>
+        <form method="post" action="">
+            <label>Username: <input type="text" name="username" /></label><br />
+            <label>Password: <input type="password" name="password" /></label><br />
+
+            <input type="submit" name="action" value="Login" />
+
+        </form>
+        <a href="<?= BASE ?>/Default/register">Register Here!</a>
+    </body>
 </html>
