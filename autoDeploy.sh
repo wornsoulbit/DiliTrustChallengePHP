8c754716c9284dfe25b753900c7ec821ad413042
Alex Vasil <alexandervasil840@gmail.com>
2022-07-15 13:06:54 -0400

    autoDeploy script that automatically updates the site

---
+++ autoDeploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#In order for this script to work you need to be in your local git repository (ACEUNIX directory)
+
+cd /home/debian/DiliTrustChallengePHP/
+
+upToDateMessage=$(git pull)
+
+
+if ! [[ $upToDateMessage == "Already up to date." ]];
+then
+	#(1)copy static files to the correct location
+	sudo rsync -r _site/* /var/www/html
+fi	
