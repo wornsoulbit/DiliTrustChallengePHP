edd7c4ae3b3e277010819a200fe45823e49effb0
Alex Vasil <alexandervasil840@gmail.com>
2022-07-15 17:42:57 -0400

    adding database connection

--- Site/index.php
+++ Site/index.php
@@ -5,6 +5,20 @@
         $password = $_POST['password'];
         echo "username: " . $username . " Password: " . $password;
     }
+
+    $servername = "localhost";
+    $DBName = 'DiliTrust';
+    $username = "username";
+    $password = "password";
+
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=$DBName", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
 ?>
 <head> <title> Landing Page </title> </head>
 <body>
