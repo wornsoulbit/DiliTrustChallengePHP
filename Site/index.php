d0285747240056cf32839f39a6fb1a09302755b1
Alex Vasil <alexandervasil840@gmail.com>
2022-07-15 16:51:21 -0400

    Decided to move away from MVC and just go with a single file.

--- Site/index.php
+++ Site/index.php
@@ -1,5 +1,17 @@
 <?php
-require "app/init.php";
 
-new App\core\App();
+//Database connection.
+$host = '127.0.0.1';
+$DBName = 'dilitrust';
+$username = 'root';
+$password = 'admin'
+
+try {
+    $conn = new \PDO("mysql:host=$host;dbname=$DBName;charset=utf-8;", $username, $password);
+    $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
+    echo "Connected Successfully!";
+} catch (PDOException $e) {
+    echo "Connection Failed: " . $e->getMessage();
+}
+
 ?>
