f3d0bc7a08e98f938e2902d0de10829a38872a9a
Alex Vasil <alexandervasil840@gmail.com>
2022-07-15 17:34:50 -0400

    test

--- Site/App/controllers/DefaultController.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\controllers;
-
-class DefaultController extends \App\core\Controller {
-    function index() {
-        $this->view('Default/index');
-    }
-}
--- Site/index.php
+++ Site/index.php
@@ -1,6 +1,14 @@
-<?php
-require "App/init.php";
-
-new App\core\App();
-
-?>
+<!DOCTYPE html>
+<head> <title> Landing Page </title> </head>
+<body>
+    <?php
+    if (isset($_GET['error']))
+        echo $_GET['error'];
+    ?>
+    <form method="post" action="">
+        <label>Username: <input type="text" name="username" /></label><br />
+        <label>Password: <input type="password" name="password" /></label><br />
+        <input type="submit" name="action" value="Login" />
+    </form>
+    <a href="<?= BASE ?>/Default/register">Register Here!</a>
+</body>
