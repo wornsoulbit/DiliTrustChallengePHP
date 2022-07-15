979d86642a06838f1964718d7784fca77f764c7e
Alex Vasil <alexandervasil840@gmail.com>
2022-07-15 16:37:31 -0400

    more debugging

--- Site/App/init.php
+++ Site/App/init.php
@@ -5,9 +5,11 @@ session_start();
 require "autoload.php";
 
 $path = getcwd();
+echo $path;
 $path = preg_replace('/^.+\\\\Site\\\\/', '/', $path);
 echo $path;
 $path = str_replace('\\', '/', $path);
+echo $path;
 define("BASE", $path);
 
 
