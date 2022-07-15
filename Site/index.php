3b88074120f228d797c4c4c81d9285ff3c56d442
Alex Vasil <alexandervasil840@gmail.com>
2022-07-15 18:31:01 -0400

    moved around db conn

--- Site/index.php
+++ Site/index.php
@@ -1,20 +1,5 @@
 <!DOCTYPE html>
 <?php 
-
-    $servername = "localhost";
-    $DBName = 'DiliTrust';
-    $username = "root";
-    $password = "admin";
-
-    try {
-    $conn = new PDO("mysql:host=$servername;dbname=$DBName", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully\n";
-    } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage() . "\n";
-    }
-
     if (isset($_POST['Login'])) {
         echo "Login Attempt\n";
         // Call login verification function
@@ -30,6 +15,21 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
         echo "username: " . $username . " Password: " . $password . "\n";
+    }
+
+    
+    $servername = "localhost";
+    $DBName = 'DiliTrust';
+    $username = "root";
+    $password = "admin";
+
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=$DBName", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully\n";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage() . "\n";
     }
 
     function verifyLogin() {
